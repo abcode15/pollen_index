75701d774b1e1d364e212803d51b153f33df8d1f
abcode15 <abcode15@163.com>
2024-05-20 19:52:54 +0800

    change color

--- pollen_web.py
+++ pollen_web.py
@@ -248,7 +248,6 @@ def make_pollen_map(input_df, input_gdf, input_date):
 def make_bar(names, values, label, x, y):
     # Figure Size
     fig, ax = plt.subplots(figsize =(x, y))
-    fig.patch.set_facecolor('grey')
     
     # Horizontal Bar Plot
     color = 'lightblue'
